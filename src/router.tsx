262f2757df6677058199dc971186661924107f04
yuri295 <jin_astro0302@naver.com>
2024-02-11 19:09:54 +0900

    test: test

---
+++ src/router.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+
+export const Router: FC = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
+};
